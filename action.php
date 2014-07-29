f21301a8a86bbe0126324025601392292c7b5d0b
RobinBeismann <robin@beismann.biz>
2014-07-29 21:15:12 +0200

    Optimized a few lines

--- action.php
+++ action.php
@@ -9,39 +9,39 @@
 		array_shift($argv);
 	}
 	
+	$argv = array_map('strtolower', $argv);
+	$str = implode(" ", $argv);
+	
 	function voiceOutput($outstr,$end)
 	{
-		$outstr = str_replace("ü","ue",$outstr);
-		$outstr = str_replace("ä","ae",$outstr);
-		$outstr = str_replace("ö","oe",$outstr);
-		system("mpg123 \"http://translate.google.com/translate_tts?tl=de&q=". $outstr . "\";");
+		$outstr = urlencode($outstr);
+		system("mpg123 \"http://translate.google.com/translate_tts?tl=de&ie=UTF-8&q=". $outstr . "\";");
 		if(isset($end)){ die($outstr); }
 	}
 	
-	foreach ($argv as &$v) {
-		$v = strtolower($v);
+	function voiceFound($s)
+	{
+		global $str;
+		return (strpos($str,$s) !== false);
 	}
 	
 	# Einfache Temperaturausgabe
-	if($argv[0] == "wetter")
+	if(voiceFound("wetter"))
 	{
-		if(!isset($argv[1]))
-		{
-			voiceOutput("Wetter wurde aufgerufen, allerdings wurde kein Tag übergeben.",true);
-		}
 
 		$loc = "GMXX1935"; # Ort (in dem Fall Freden(Leine)
 		$weather_arr = parse_ini_string(file_get_contents("http://weather.tuxnet24.de/?id=" . $loc));
 	
-		if($argv[1] == "heute")
+		if(voiceFound("heute"))
 		{
 			voiceOutput("In " . $weather_arr["city"] . " sind es heute " . str_replace("°C", "Grad Celsius",$weather_arr["current_temp"]),true);
 		}
-		elseif($argv[1] == "morgen")
+		elseif(voiceFound("morgen"))
 		{
 			voiceOutput("In " . $weather_arr["city"] . " werden es morgen mindestens " . str_replace("°C", "Grad Celsius",$weather_arr["tomorrow_temp_low"]) . " und maximal " . str_replace("°C", "Grad Celsius",$weather_arr["tomorrow_temp_high"]),true);
 		}
-
+		
+		voiceOutput("Wetter wurde aufgerufen, allerdings wurde kein Tag übergeben.",true);
 	}	
 	
 ?>
