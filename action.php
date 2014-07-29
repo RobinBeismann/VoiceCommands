ff7951d9ac8de0f32550e076d208d0d8f972c423
RobinBeismann <robin@beismann.biz>
2014-07-29 19:25:04 +0200

    Added a loop to put all args into lowercase

--- action.php
+++ action.php
@@ -14,8 +14,13 @@
 		system("mpg123 \"http://translate.google.com/translate_tts?tl=de&q= ". $outstr . "\";");
 	}
 	
-	## Einfache Temperaturausgabe
-	if(strtolower($argv[0]) == "wetter")
+	# Changing all args to lowercase to make if statements easier
+	foreach ($argv as &$v) {
+		$v = strtolower($v);
+	}
+	
+	# Einfache Temperaturausgabe
+	if($argv[0] == "wetter")
 	{
 		if(!isset($argv[1]))
 		{
@@ -25,11 +30,11 @@
 		$loc = "GMXX1935"; # Ort (in dem Fall Freden(Leine)
 		$weather_arr = parse_ini_string(file_get_contents("http://weather.tuxnet24.de/?id=" . $loc));
 	
-		if(strtolower($argv[1]) == "heute")
+		if($argv[1] == "heute")
 		{
 			voiceOutput("In " . $weather_arr["city"] . " sind es heute " . str_replace("°C", "Grad Celsius",$weather_arr["current_temp"]));
 		}
-		elseif(strtolower($argv[1]) == "morgen")
+		elseif($argv[1] == "morgen")
 		{
 			voiceOutput("In " . $weather_arr["city"] . " werden es morgen mindestens " . str_replace("°C", "Grad Celsius",$weather_arr["tomorrow_temp_low"]) . " und maximal " . str_replace("°C", "Grad Celsius",$weather_arr["tomorrow_temp_high"]));
 		}
