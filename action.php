ba2d8f6d449f459987ba4d9a5831acebb3c0d2c7
RobinBeismann <robin@beismann.biz>
2014-07-29 19:41:10 +0200

    Fixed Handling of German vowel mutations

--- action.php
+++ action.php
@@ -9,12 +9,15 @@
 		array_shift($argv);
 	}
 	
-	function voiceOutput($outstr)
+	function voiceOutput($outstr,$end)
 	{
-		system("mpg123 \"http://translate.google.com/translate_tts?tl=de&q= ". $outstr . "\";");
+		$outstr = str_replace("ü","ue",$outstr);
+		$outstr = str_replace("ä","ae",$outstr);
+		$outstr = str_replace("ö","oe",$outstr);
+		system("mpg123 \"http://translate.google.com/translate_tts?tl=de&q=". $outstr . "\";");
+		if(isset($end)){ die($outstr); }
 	}
 	
-	# Changing all args to lowercase to make if statements easier
 	foreach ($argv as &$v) {
 		$v = strtolower($v);
 	}
@@ -24,7 +27,7 @@
 	{
 		if(!isset($argv[1]))
 		{
-			die("Wetter wurde aufgerufen, allerdings wurde kein Tag übergeben.");
+			voiceOutput("Wetter wurde aufgerufen, allerdings wurde kein Tag übergeben.",true);
 		}
 
 		$loc = "GMXX1935"; # Ort (in dem Fall Freden(Leine)
@@ -32,11 +35,11 @@
 	
 		if($argv[1] == "heute")
 		{
-			voiceOutput("In " . $weather_arr["city"] . " sind es heute " . str_replace("°C", "Grad Celsius",$weather_arr["current_temp"]));
+			voiceOutput("In " . $weather_arr["city"] . " sind es heute " . str_replace("°C", "Grad Celsius",$weather_arr["current_temp"]),true);
 		}
 		elseif($argv[1] == "morgen")
 		{
-			voiceOutput("In " . $weather_arr["city"] . " werden es morgen mindestens " . str_replace("°C", "Grad Celsius",$weather_arr["tomorrow_temp_low"]) . " und maximal " . str_replace("°C", "Grad Celsius",$weather_arr["tomorrow_temp_high"]));
+			voiceOutput("In " . $weather_arr["city"] . " werden es morgen mindestens " . str_replace("°C", "Grad Celsius",$weather_arr["tomorrow_temp_low"]) . " und maximal " . str_replace("°C", "Grad Celsius",$weather_arr["tomorrow_temp_high"]),true);
 		}
 
 	}	
